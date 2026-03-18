73ecc937f15d3f69af202629ed20d86533b3a6e4
Ayush Dharaiya <139906238+Ayush2029@users.noreply.github.com>
2026-03-18 22:22:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,12 +5,10 @@ const nextConfig = {
   },
   webpack: (config, { isServer }) => {
     if (isServer) {
-      // pdfjs-dist uses canvas as an optional dependency — ignore it
       config.resolve.alias.canvas = false;
       config.resolve.alias.encoding = false;
     }
     return config;
   },
 };
-
 module.exports = nextConfig;
