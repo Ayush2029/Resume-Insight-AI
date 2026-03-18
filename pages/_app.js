812c345da54acdd2a67e2ddaaeb40e897c145e49
Ayush Dharaiya <139906238+Ayush2029@users.noreply.github.com>
2026-03-18 22:15:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import Head from 'next/head';
 import { Fira_Code, DM_Sans } from 'next/font/google';
 
 const firaCode = Fira_Code({
@@ -24,15 +23,8 @@ const monoVar = Fira_Code({
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
-        <meta name="theme-color" content="#0a0c10" />
-        <title>Resume Insight AI</title>
-      </Head>
-      <div className={`${firaCode.variable} ${dmSans.variable} ${monoVar.variable}`}>
-        <Component {...pageProps} />
-      </div>
-    </>
+    <div className={`${firaCode.variable} ${dmSans.variable} ${monoVar.variable}`}>
+      <Component {...pageProps} />
+    </div>
   );
 }
